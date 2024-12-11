423298fd8e9a5940cddd49c70f7079c4a940760d
matthewcornell12 <mrcorn@seas.upenn.edu>
2024-12-11 17:22:48 -0500

    implement borrow funds method

--- contracts/core/LendingPool.sol
+++ contracts/core/LendingPool.sol
@@ -5,6 +5,9 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 
 contract LendingPool is ReentrancyGuard, Pausable {
+    // Constants
+    uint256 constant MIN_COLLATERAL_RATIO = 150; // 150% collateralization required
+
     // State Variables
     mapping (address => uint256) public userCollateral;
     uint256 public totalCollateral;
@@ -16,6 +19,9 @@ contract LendingPool is ReentrancyGuard, Pausable {
     // Event definitions
     event Deposit(address indexed user, uint256 amount);
     event Withdrawal(address indexed user, uint256 amount);
+
+    event FundsBorrowed(address indexed user, uint256 amount);
+    event FundsRepaid(address indexed user, uint256 amount, uint256 balanceRemaining);
 
     // Function definitions
     function deposit() public payable nonReentrant {
@@ -35,5 +41,19 @@ contract LendingPool is ReentrancyGuard, Pausable {
         (bool success, ) = msg.sender.call{value: amount}("");
         require(success, "Withdrawal Failed");
         emit Withdrawal(msg.sender, amount);
+    }
+
+    function borrowFunds(uint256 amount) public nonReentrant {
+        uint256 userTotalBorrowed = userBorrowed[msg.sender] + amount;
+        require(userCollateral[msg.sender] * 100 >=  userTotalBorrowed * MIN_COLLATERAL_RATIO, "User does not have enough collateral for this loan");
+        require(amount > 0, "Must borrow more than 0 ETH");
+        require(address(this).balance > amount, "Contract does not have enough funds for this loan");
+
+        userBorrowed[msg.sender] += amount;
+        totalBorrowed += amount;
+
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require (success, "Loan failed");
+        emit FundsBorrowed(msg.sender, amount);
     }
 }
